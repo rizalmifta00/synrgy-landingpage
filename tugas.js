0805efd129a67f388b0bd8d9f257956936ae08d8
rizalmifta79 <rizalmifta79@gmail.com>
2022-01-18 18:44:48 +0700

    add tugas flowchart

---
+++ tugas.js
@@ -0,0 +1,21 @@
+const linkFlowchart ="https://whimsical.com/flowchart-W6hfTuQwb5QB5sMwF4EhS3"
+let beliBarang = true;
+let bayar = true;
+console.log("login")
+console.log("mencari barang yang diinginkan")
+console.log("Memilih barang")
+console.log("halaman keranjang")
+console.log("pilih checkout")
+if(beliBarang==true){
+    console.log("isi nama dan tujuan")
+}else{
+    console.log("halaman keranjang")
+}
+console.log("pilih metode bayar")
+console.log("Konfirmasi")
+
+if(bayar==true){
+    console.log("Selesai")
+}else{
+    console.log("masuk halaman keranjag")
+}

2918b34af7a24394e019b8f83e09864b0550dbeb
rizalmifta79 <rizalmifta79@gmail.com>
2022-01-21 16:53:01 +0700

    add tugas condition looping and function

---
+++ function.js
@@ -0,0 +1,81 @@
+var arr = [2, 5, 10, 'ini dia', 'data', 100, 38]
+
+// function data(number) {
+//     if (number.length < 50) {
+//         for (var i = 0; i < number.length; i++) {
+//             console.log(number[i])
+//         }
+//     } else {
+//         console.log(true)
+//     }
+// }
+// data(arr)
+
+var arr2 = [3, 100, 283, 'saya', 'adalah', 'kapiten', 75]
+
+function data2(number1, number2) {
+    if (number1 + number2.length != 32) {
+        for (var i = 0; i < number2.length; i++) {
+            console.log(number2[i])
+        }
+    } else {
+        console.log(false)
+    }
+}
+data2(25, arr2)
+
+
+
+
+var arr3 = [29, 12, 30, 'ini', 'saya', 85]
+
+// function data3(a, number2) {
+//     if (number2.length + a == 28) {
+//         for (var i = 0; i < number2.length; i++) {
+//             console.log(number2[i])
+//         }
+//     } else {
+//         console.log(false)
+//     }
+// }
+// data3(21, arr3)
+
+var arr4 = [20, 12, 43, 54, 24]
+
+// function data4(number2) {
+//     if (number2.length + 1 != 35) {
+//         for (var i = 0; i < number2.length; i++) {
+//             console.log(number2[i])
+//         }
+//     } else {
+//         console.log(false)
+//     }
+// }
+
+// data4(arr4)
+
+var arr5 = [20, 'empat', 'limat', 8, 29, 30]
+
+// function data5(x, number2) {
+//     if (x + number2.length != 35) {
+//         for (var i = 0; i < number2.length; i++) {
+//             console.log(number2[i])
+//         }
+//     } else {
+//         console.log(false)
+//     }
+// }
+// data5(29, arr5)
+
+// var arr6 = ['data', 'empat', 8, 3, 100, 8, 100, 3]
+
+// function data6(a, c, number2) {
+//     if (c + number2.length + a != 35) {
+//         for (var i = 0; i < number2.length; i++) {
+//             console.log(number2[i])
+//         }
+//     } else {
+//         console.log(false)
+//     }
+// }
+// data6(20, 7, arr6)
---
+++ if-else-challange.js
@@ -0,0 +1,80 @@
+let a = 50;
+let b = 31;
+let c = 0;
+let x = 32;
+let y = 3;
+
+// for (i = 0; i <= 100; i++) {
+//     if (a == 50) {
+//         console.log(i)
+//     } else {
+//         console.log("true")
+//         break;
+//     }
+// }
+
+if (a == 50) {
+    while (a) {
+        console.log(a)
+        a--;
+    };
+} else {
+    console.log("true")
+}
+
+if (b + a != 32) {
+    while (b) {
+        console.log(b);
+        b--;
+    }
+} else {
+    console.log(true)
+}
+
+// 3
+if (c + a == 28) {
+    while (c) {
+        console.log(c);
+        c--;
+    }
+} else {
+    console.log(true)
+}
+
+// 4
+if (x == 35) {
+    while (x) {
+        console.log(x)
+        x--;
+    }
+} else {
+    console.log(true)
+}
+
+// 5
+x=2
+y=32
+if (x + y != 35) {
+    while (y) {
+        console.log(y);
+        y--;
+    }
+} else {
+    console.log(true);
+}
+
+// 6
+c=0;
+y=2;
+a=20;
+if (c + y + a != 25) {
+    while (a) {
+        console.log(a)
+        a--;
+    }
+} else {
+    console.log(true)
+}
+
+
+
